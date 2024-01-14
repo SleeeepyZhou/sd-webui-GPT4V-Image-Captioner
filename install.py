5feddfe45f650525683c24574ca39ba983f6bf8a
周公不解梦 <sleeeepyzhou@yeah.net>
2024-01-14 15:26:22 +0800

    sd webui extension install

---
+++ install.py
@@ -0,0 +1,14 @@
+﻿import os
+import launch
+
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+
+req_file = os.path.join(BASE_PATH, "requirements.txt")
+
+with open(req_file) as file:
+    for lib in file:
+        lib = lib.strip()
+        if not launch.is_installed(lib):
+            launch.run_pip(
+                f"install {lib}",
+                f"sd-webui-segment-anything requirement: {lib}")
